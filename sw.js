c3ff1287f757b355a73a89f170a51306797183df
CourseDesignUK <gibbo216@gmail.com>
2026-01-04 19:25:51 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,25 @@
+const CACHE_NAME = 'mind-garden-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './icon.png',
+  './storm.mp3',
+  './waves.mp3',
+  './forest.mp3',
+  './ambient.mp3',
+  './birdsong.mp3'
+];
 
+// Install: Cache all UI and Audio files
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Fetch: Serve from cache if offline
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
